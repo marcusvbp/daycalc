6425a9033aeecb19b06d53750c37bb5989b1604d
Marcus VBP <marcusviniciusbp@gmail.com>
2025-09-09 16:04:59 -0300

    Refatora o método formatHoursToString na classe DateOperationRecord para utilizar a função hoursToString, que melhora a formatação das horas em uma string legível, e adiciona suporte para o código de idioma.

--- lib/app/models/date_operation_record.dart
+++ lib/app/models/date_operation_record.dart
@@ -1,5 +1,6 @@
 import 'package:daycalc/app/enums/operation_type.dart';
 import 'package:daycalc/app/enums/time_unit.dart';
+import 'package:daycalc/utils/format_hours_to_string.dart';
 
 /// Modelo de dados para um registro de operação no histórico
 class DateOperationRecord {
@@ -43,33 +44,9 @@ class DateOperationRecord {
   }
 
   /// Formata as horas em uma string legível
-  String formatHoursToString() {
+  String formatHoursToString(String languageCode) {
     final units = formatHoursToUnits();
-    final List<String> parts = [];
-
-    if (units['years']! > 0) {
-      parts.add('${units['years']} ${units['years'] == 1 ? 'ano' : 'anos'}');
-    }
-    if (units['months']! > 0) {
-      parts.add('${units['months']} ${units['months'] == 1 ? 'mês' : 'meses'}');
-    }
-    if (units['weeks']! > 0) {
-      parts.add(
-        '${units['weeks']} ${units['weeks'] == 1 ? 'semana' : 'semanas'}',
-      );
-    }
-    if (units['days']! > 0) {
-      parts.add('${units['days']} ${units['days'] == 1 ? 'dia' : 'dias'}');
-    }
-    if (units['hours']! > 0) {
-      parts.add('${units['hours']} ${units['hours'] == 1 ? 'hora' : 'horas'}');
-    }
-
-    if (parts.isEmpty) {
-      return '0 horas';
-    }
-
-    return parts.join(', ');
+    return hoursToString(units, languageCode);
   }
 
   @override
