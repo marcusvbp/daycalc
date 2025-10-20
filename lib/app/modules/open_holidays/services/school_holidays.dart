8818b3488ab21ca41b3bda6914e7a8d4a50b0521
Marcus VBP <marcusviniciusbp@gmail.com>
2025-10-20 09:05:49 -0300

    refactor(open_holidays): replace SchoolHoliday with PublicHoliday model
    The SchoolHoliday model was removed and replaced with PublicHoliday to consolidate holiday types and reduce code duplication. All related service methods were updated to use the new model.

--- lib/app/modules/open_holidays/services/school_holidays.dart
+++ lib/app/modules/open_holidays/services/school_holidays.dart
@@ -1,8 +1,8 @@
+import 'package:daycalc/app/modules/open_holidays/api_routes.dart';
+import 'package:daycalc/app/modules/open_holidays/models/public_holiday.dart';
+import 'package:daycalc/app/providers/http_client_provider.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:daycalc/app/modules/open_holidays/api_routes.dart';
-import 'package:daycalc/app/providers/http_client_provider.dart';
-import 'package:daycalc/app/modules/open_holidays/models/school_holiday.dart';
 
 class SchoolHolidaysService {
   final Ref ref;
@@ -13,25 +13,25 @@ class SchoolHolidaysService {
     _http = ref.read(httpClientProvider);
   }
 
-  Future<List<SchoolHoliday>> listSchoolHolidays(
+  Future<List<PublicHoliday>> listSchoolHolidays(
     Map<String, dynamic> queryParameters,
   ) async {
     final path = _routes.getRoute('school-holidays');
     final response = await _http.get(path!, queryParameters: queryParameters);
     final data = response.data as List<dynamic>;
     return data
-        .map((e) => SchoolHoliday.fromJson(e as Map<String, dynamic>))
+        .map((e) => PublicHoliday.fromJson(e as Map<String, dynamic>))
         .toList();
   }
 
-  Future<List<SchoolHoliday>> listSchoolHolidaysByDate(
+  Future<List<PublicHoliday>> listSchoolHolidaysByDate(
     Map<String, dynamic> queryParameters,
   ) async {
     final path = _routes.getRoute('school-holidays-by-date');
     final response = await _http.get(path!, queryParameters: queryParameters);
     final data = response.data as List<dynamic>;
     return data
-        .map((e) => SchoolHoliday.fromJson(e as Map<String, dynamic>))
+        .map((e) => PublicHoliday.fromJson(e as Map<String, dynamic>))
         .toList();
   }
 }
