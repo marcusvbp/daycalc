237f3c7e176c286e2c24d420462a55d1e82f8911
Marcus VBP <marcusviniciusbp@gmail.com>
2025-10-27 20:05:26 -0300

    feat(ads): add holiday ad unit IDs for Android and iOS
    Add new AdMob ad unit IDs for holiday feature in both local and release environments, and update constants to support the new ad units

--- lib/app/config/constants.dart
+++ lib/app/config/constants.dart
@@ -8,6 +8,9 @@ const admobInterstitialIos = String.fromEnvironment('ADMOB_INTERSTITIAL_IOS');
 const admobHomeAndroid = String.fromEnvironment('ADMOB_HOME_ANDROID');
 const admobHomeIos = String.fromEnvironment('ADMOB_HOME_IOS');
 
+const admobFeriadoAndroid = String.fromEnvironment('ADMOB_FERIADO_ANDROID');
+const admobFeriadoIos = String.fromEnvironment('ADMOB_FERIADO_IOS');
+
 final admobHomeId = Platform.isAndroid ? admobHomeAndroid : admobHomeIos;
 
 final admobInterstitialId = Platform.isAndroid
@@ -20,6 +23,10 @@ const admobHistoricoIos = String.fromEnvironment('ADMOB_HISTORICO_IOS');
 final admobHistoricoId = Platform.isAndroid
     ? admobHistoricoAndroid
     : admobHistoricoIos;
+
+final admobFeriadoId = Platform.isAndroid
+    ? admobFeriadoAndroid
+    : admobFeriadoIos;
 
 const openHolidaysApiUrl = String.fromEnvironment('OPEN_HOLIDAYS_API_URL');
 const openHolidaysMaxInterval = int.fromEnvironment(
